91db1a3b209df0812eb923a66e4b8201858d4367
HelenEVA <92659299+HelenEVA@users.noreply.github.com>
2022-02-06 22:24:15 +0700

    lesson16

--- src/Ex16.java
+++ src/Ex16.java
@@ -1,2 +1,18 @@
 public class Ex16 {
+
+    public static boolean isGreenLight = false;
+
+    public static boolean getSpeed(int speedOfPlayer) {
+        boolean Speed = false;
+        if (speedOfPlayer > 0) {
+            if (!isGreenLight)
+                Speed = true;
+        } else {
+            Speed = false;
+        }
+        return Speed;
+
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean isGreenLight = false;
 
-        int speedOfPlayer1 = 5;
-        int speedOfPlayer2 = 9;
-        int speedOfPlayer3 = 3;
-
-        int retired = 0;
-
-
-        if (isGreenLight) { //если зеленый
-            System.out.println("все проходят");
-        } else { //если не зеленый
-            if (speedOfPlayer1 > 0)
-                retired = retired + 1;
-
-            if (speedOfPlayer2 > 0)
-                retired = retired + 1;
-
-            if (speedOfPlayer3 > 0)
-                retired = retired + 1;
-
+        Ex16.isGreenLight = true;
+        if (Ex16.getSpeed(9)) {
+            System.out.println("Игрок выбыл");
+        } else {
+            System.out.println("Игрок проходит");
         }
-
-        System.out.println("Выбыло игроков: " + retired);
-        // Допишите здесь логику так, что будет подсчитано и выведено
-        // количество игроков, которые выбывают.
-        // Если свет зелёный, то проходят все игроки (0 выбывают).
-        // Если свет красный, то выбывает каждый, чья скорость не 0.
     }
 
 }
